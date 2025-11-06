20404dc60668306a8f3aad1df0e8dc9892ec440c
presabkhadka30 <presabkhadka30@gmail.com>
2025-11-06 17:38:23 +0545

    vue lsp & formatter for vue

---
+++ lua/plugins/lsp-vue.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        volar = {
+          filetypes = { "vue" },
+          init_options = {
+            typescript = {
+              -- Use your local TypeScript SDK (important)
+              tsdk = vim.fn.expand(
+                "~/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib"
+              ),
+            },
+          },
+          -- prevent timeouts on large files
+          flags = { debounce_text_changes = 150 },
+          on_attach = function(client, bufnr)
+            -- disable formatting if you use prettier
+            client.server_capabilities.documentFormattingProvider = false
+          end,
+        },
+      },
+    },
+  },
+}
