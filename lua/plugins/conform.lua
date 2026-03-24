05edfc61c150f94e74c94a83ae5617854200b207
Presab Khadka <presabkhadka30@gmail.com>
2026-03-24 21:15:25 +0545

    chore: removing linter config for vue

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,6 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        vue = { "prettierd", "prettier" },
         javascript = { "prettierd", "prettier" },
         typescript = { "prettierd", "prettier" },
         html = { "prettierd", "prettier" },
@@ -13,7 +12,7 @@ return {
       },
       format_on_save = function(bufnr)
         -- Disable autoformat for files in node_modules
-        if vim.bo[bufnr].filetype == "vue" or vim.bo[bufnr].filetype == "javascript" then
+        if vim.bo[bufnr].filetype == "javascript" or vim.bo[bufnr].filetype == "typescript" then
           return true
         end
       end,
